87ccd28071c31445f61112692b67ca8672dec087
Dan Winter <dan.j.winter@gmail.com>
2015-12-21 16:12:10 -0700

    added email validation for only one period

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -40,7 +40,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@exaple. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[user@example,com foo@bar..com user_at_foo.org user.name@exaple. foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -54,6 +54,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as lower case" do
+    mixed_case_email = "Foo@ExaMPle.cOM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
@@ -63,4 +70,6 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+
 end
